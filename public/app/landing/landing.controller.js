bb169c95f8779fa7bf4034eed443a6b9b5480a4a
Michelle <mtham101@gmail.com>
2016-07-15 20:56:25 -0700

    implement simple scroll box

--- public/app/landing/landing.controller.js
+++ public/app/landing/landing.controller.js
@@ -8,6 +8,17 @@
   function LandingCtrl () {
     var vm = this;
     vm.data = 'World!';
+    vm.title = 'Simple Scrollbox';
+    vm.image = '../assets/reddit.png'
+    vm.list = [];
+    for(var i = 0; i < 15; i++) {
+      vm.list.push({
+        face: vm.image,
+        what: 'Reddit',
+        who: 'ents',
+        notes: 'When does the norwhal bacon?'
+      })
+    }
   }
 
 })();
